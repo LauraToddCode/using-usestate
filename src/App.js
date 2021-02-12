0f8e8f1b4f40e941992f60d8b119df56aad15ab2
LauraToddCode <laura20todd@gmail.com>
2021-02-12 10:38:23 +0000

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+    const [ name, setName ] = useState("my name");
+
+    const handleChange = (event) => {
+        setName(event.target.value)
+    }
+
+    return (
+        <div>
+            <div>
+                <label>Type your name:</label>
+                <input type="text" onChange={handleChange}/>
+            </div>
+
+            
+            <p>{name}</p>
+        </div>
+    )
 }
 
-export default App;
+export default App
